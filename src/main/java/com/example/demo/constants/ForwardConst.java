13b836263ec5150b1b8962331e0e5d5b154fd334
kudou.sho <kudou.sho0307@gmail.com>
2022-06-03 21:37:50 +0900

    ForwardConstを作成

---
+++ src/main/java/com/example/demo/constants/ForwardConst.java
@@ -0,0 +1,23 @@
+package com.example.demo.constants;
+
+// 画面遷移に関する値を定義するファイル
+public interface ForwardConst {
+
+    // html
+    // エラー画面
+    String ERR_UNKNOWN_PAGE = "views/error/unknown";
+
+    // アカウント
+    String ACCOUNT_INDEX_PAGE = "views/accounts/index";
+    String ACCOUNT_EDIT_PAGE = "views/accounts/edit";
+
+    // アカウント新規作成
+    String ACCOUNT_NEW_CREATE_PAGE = "views/accounts/new-create";
+
+    // メール
+    String SEND_MAIL_COMPLETE = "views/mails/send-complete";
+
+
+
+
+}
