01e4dae42c4009677d5fff5497bb82339edd752b
kudou.sho <kudou.sho0307@gmail.com>
2022-06-03 21:37:38 +0900

    MessageConstを作成

---
+++ src/main/java/com/example/demo/constants/MessageConst.java
@@ -0,0 +1,14 @@
+package com.example.demo.constants;
+
+// メッセージに関する文字を定義するファイル
+public interface MessageConst {
+
+    // アカウント新規作成
+    String ACCOUNT_ALREADY_EXISTS = "入力されたメールアドレスは既に登録されています。";
+
+
+    // バリデーションエラーメッセージ
+
+    // メールアドレス
+    String VALID_EMAIL = "メールアドレスを正しく入力してください";
+}
