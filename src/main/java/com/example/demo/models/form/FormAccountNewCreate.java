43f9733dbea736eea78bebb5e861038afae7af43
kudou.sho <kudou.sho0307@gmail.com>
2022-06-07 11:33:00 +0900

    FormAccountNewCreate作成

---
+++ src/main/java/com/example/demo/models/form/FormAccountNewCreate.java
@@ -0,0 +1,31 @@
+package com.example.demo.models.form;
+
+import java.io.Serializable;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+
+import com.example.demo.constants.MessageConst;
+import com.example.demo.models.OnetimePassword;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class FormAccountNewCreate implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    // id
+    private Integer id;
+
+    // メールアドレス
+    @NotBlank(message=MessageConst.VALID_EMAIL)
+    @Email(message=MessageConst.VALID_EMAIL)
+    private String email;
+
+    // ワンタイムパスワードテーブルid
+    private OnetimePassword otp;
+}
