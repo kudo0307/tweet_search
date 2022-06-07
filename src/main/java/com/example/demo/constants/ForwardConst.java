6d4f8441a2b861fd8ddc1dc58be5126fe1e95d14
kudou.sho <kudou.sho0307@gmail.com>
2022-06-07 11:28:54 +0900

    エラー画面、アカウント新規作成のURL追加

--- src/main/java/com/example/demo/constants/ForwardConst.java
+++ src/main/java/com/example/demo/constants/ForwardConst.java
@@ -6,16 +6,18 @@ public interface ForwardConst {
     // html
     // エラー画面
     String ERR_UNKNOWN_PAGE = "views/error/unknown";
+    String ERR_TOKEN_PAGE = "views/error/token-error";
 
     // アカウント
     String ACCOUNT_INDEX_PAGE = "views/accounts/index";
     String ACCOUNT_EDIT_PAGE = "views/accounts/edit";
 
     // アカウント新規作成
-    String ACCOUNT_NEW_CREATE_PAGE = "views/accounts/new-create";
+    String ACCOUNT_NEW_INDEX_PAGE = "views/accounts/newcreate/index";
+    String ACCOUNT_NEW_CREATE_PAGE = "views/accounts/newcreate/create";
+    String ACCOUNT_NEW_CREATE_COMPLETE = "views/accounts/newcreate/complete";
+    String ACCOUNT_NEW_CREATE_SEND_MAIL = "views/accounts/newcreate/send-mail";
 
-    // メール
-    String SEND_MAIL_COMPLETE = "views/mails/send-complete";
 
 
 
