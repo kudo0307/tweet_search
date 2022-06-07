a764cd128c6a9f2a1dd9d7c7aa1a9b3c9837bf59
kudou.sho <kudou.sho0307@gmail.com>
2022-06-07 11:36:00 +0900

    queryを追加

--- src/main/java/com/example/demo/repositorys/AccountRepository.java
+++ src/main/java/com/example/demo/repositorys/AccountRepository.java
@@ -3,11 +3,23 @@ package com.example.demo.repositorys;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.example.demo.models.Account;
 
 @Repository
 public interface AccountRepository extends JpaRepository<Account,Integer> {
-    List<Account> findByDeletedAtIsNull();
+    // 削除されていないアカウントデータを取得
+    @Query(value="SELECT a FROM Account AS a WHERE a.deletedAt IS NULL ORDER BY a.id DESC")
+    List<Account> findAllByDeletedAtIsNullOrderDescById();
+
+    // idを元に削除されていないアカウントデータを取得
+    @Query(value="SELECT a FROM Account AS a WHERE a.deletedAt IS NULL AND a.id = :id")
+    Account findByDeletedAtIsNullById(@Param("id") Integer id);
+
+    // メールアドレスを元に削除されていないアカウントデータを取得
+    @Query(value="SELECT a FROM Account AS a WHERE a.deletedAt IS NULL AND a.email = :email")
+    Account findByDeletedAtIsNullANDEmailIs(String email);
 }
