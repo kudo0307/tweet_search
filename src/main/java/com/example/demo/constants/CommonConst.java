40030208a30082ff2f772b72362e93727f3553fd
kudou.sho <kudou.sho0307@gmail.com>
2022-06-07 11:16:35 +0900

    CommonConstに値を追加

--- src/main/java/com/example/demo/constants/CommonConst.java
+++ src/main/java/com/example/demo/constants/CommonConst.java
@@ -7,8 +7,4 @@ public interface CommonConst {
     String OAUTH_CONSUMER_KEY = "lYHg71gP1OMQdoMVdUvvAPQKQ";
     String OAUTH_CONSUMER_SECRET = "46KZpVpO9tJjdNExFdkJQBvAnlszqLPCDAaaw7FHtQyaPvbhXo";
     String BEARER_TOKEN = "AAAAAAAAAAAAAAAAAAAAAFkzcwEAAAAAN25jW82k8G5PB483GL9fuoBZuI0%3D2ZqaqMxY5UwoRVpoxk1pwcUyMquQldteyYEi9AwQ27TEOlE4hH";
-
-    // 管理者権限
-    int ROLE_ADMIN = 1; // 管理者
-    int ROLE_GENERAL = 0; // 一般
 }
