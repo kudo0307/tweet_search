73d1a61b34a7b6bae1eed51380f28e199b6b1c44
kudou.sho <kudou.sho0307@gmail.com>
2022-06-07 11:31:23 +0900

    バリデーションメッセージとメールに使用する文章を追加

--- src/main/java/com/example/demo/constants/MessageConst.java
+++ src/main/java/com/example/demo/constants/MessageConst.java
@@ -11,4 +11,24 @@ public interface MessageConst {
 
     // メールアドレス
     String VALID_EMAIL = "メールアドレスを正しく入力してください";
+    String VALID_NAME = "アカウント名を正しく入力してください";
+
+    // パスワード
+    String VALID_PASSWORD_NOT_BLANK = "パスワードは必須です";
+    String VALID_PASSWORD_MIN = "パスワードは"+JpaConst.PASSWORD_MIN + "文字以上で入力してください";
+    String VALID_PASSWORD_CONFIRM = "パスワードと再入力パスワードが一致しません";
+
+
+    // メール
+    String MAIL_URL_PATH = "http://localhost:8080";
+    // from
+    String MAIL_FROM = "tweetsearchinfo@gmail.com";
+    // cc
+    String MAIL_CC = "tweetsearchinfo@gmail.com";
+    // アカウント新規作成
+    String ACCOUNT_NEW_CREATE_MAIL_SUBJECT = "アカウント新規作成手続き"; // 件名
+    String ACCOUNT_NEW_CREATE_MAIL_TEXT =
+            "下記にて本登録のお手続きをお願い致します。\n\n"
+            + "URL : " + MAIL_URL_PATH + "/accountNewCreate/create?token=${token}"; // 本文
+
 }
