cf667984e82ad49aacb6fd06fc05bb96fa0ced97
kudou.sho <kudou.sho0307@gmail.com>
2022-06-17 21:46:22 +0900

    二重送信防止トークン追加

--- src/main/java/com/example/demo/constants/CommonConst.java
+++ src/main/java/com/example/demo/constants/CommonConst.java
@@ -10,4 +10,10 @@ public interface CommonConst {
 
     // tweet検索時の取得件数
     String TWEET_MAX_RESULT = "10";
+
+    // 二重送信防止トークン
+    // 生成文字数
+    int PROTECTION_TOKEN_INT = 20;
+    // 文字列
+    String PROTECTION_TOKEN_STR = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
 }
