0943b95f3279433517f5693582eacb301116d622
kudou.sho <kudou.sho0307@gmail.com>
2022-06-17 21:53:38 +0900

    メインクラスとテストクラス

---
+++ src/main/java/com/example/demo/TweetSearchApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TweetSearchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TweetSearchApplication.class, args);
+    }
+
+}
