18013dadee9afb8c818d14377d546a4fcb6b8e31
kudou.sho <kudou.sho0307@gmail.com>
2022-06-21 22:02:35 +0900

    ログアウト時はリダイレクトをするように修正

--- src/main/java/com/example/demo/actions/AuthAction.java
+++ src/main/java/com/example/demo/actions/AuthAction.java
@@ -2,8 +2,8 @@ package com.example.demo.actions;
 
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.example.demo.constants.ForwardConst;
 import com.example.demo.constants.MessageConst;
@@ -17,11 +17,11 @@ public class AuthAction extends  ActionBase{
     }
 
     @GetMapping("/logout")
-    public String logout(Model model) {
+    public String logout(RedirectAttributes redirectAttributes) {
         // セッションを破棄
         SecurityContextHolder.clearContext();
-        model.addAttribute("logout",MessageConst.LOGOUT_SUCCESS);
-        return ForwardConst.LOGIN_PAGE;
+        redirectAttributes.addFlashAttribute("logout",MessageConst.LOGOUT_SUCCESS);
+        return "redirect:/login";
     }
 
 }
