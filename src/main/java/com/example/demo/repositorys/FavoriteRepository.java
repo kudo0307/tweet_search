5469b13d12d496c4ab929fb2c4c7e36e29052f04
kudou.sho <kudou.sho0307@gmail.com>
2022-06-28 20:12:30 +0900

    お気に入りデータを新しいデータ順に取得するように修正

--- src/main/java/com/example/demo/repositorys/FavoriteRepository.java
+++ src/main/java/com/example/demo/repositorys/FavoriteRepository.java
@@ -13,6 +13,6 @@ public interface FavoriteRepository extends JpaRepository<Favorite,Integer> {
     @Query(value="SELECT fvt FROM Favorite AS fvt WHERE fvt.twe.tweetId = :tweetId AND fvt.ac.id = :accountId")
     Favorite find(long tweetId,Integer accountId);
 
-    @Query(value="SELECT fvt FROM Favorite AS fvt WHERE fvt.flag = true AND fvt.ac.id = :accountId  ORDER BY fvt.ac.id DESC")
+    @Query(value="SELECT fvt FROM Favorite AS fvt WHERE fvt.flag = true AND fvt.ac.id = :accountId  ORDER BY fvt.id DESC")
     Page<Favorite> list(Pageable pageable,Integer accountId);
 }
